53c58ded20a17df735302e754f3ea892b5bc7df0
osama waheed salman abu mousa <60567599+osamamousa204@users.noreply.github.com>
2020-06-15 08:59:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const server = require('./lib/server');
 
-mongoose.connect(process.env.MONGODB_URI,{
+mongoose.connect('mongodb+srv://osamamousa204:1234@cluster0-yxzpk.mongodb.net/test',{
   useNewUrlParser:true,
   useUnifiedTopology:true,
 });
